a88e279b19db4125191dea3472d92658806dbecc
bingbiu <44487782+IceColdwind@users.noreply.github.com>
2023-07-13 10:32:38 +0800

    Update config.default.js
    1

--- config/config.default.js
+++ config/config.default.js
@@ -41,10 +41,10 @@ module.exports = appInfo => {
   };
 
   config.userData = {
-    mineBirth: "1994-03-24", // 自己的生日
-    gfBirth: "1994-12-26", // 女朋友的生日
-    loveDay: "2007-08-11", // 在一起的日期
-    weatherCity: '茂名市' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
+    mineBirth: "1999-03-21", // 自己的生日
+    gfBirth: "1998-12-21", // 女朋友的生日
+    loveDay: "2023-06-16", // 在一起的日期
+    weatherCity: '合肥市' // 需要获取天气的城市，必须时xx市，xx县，xx自治区, 详细可以去utils/amap.js搜索到就可以，比如广州市，不能是广州
   }
 
   config.userCity = {
